84ee5fc4aafc47e0500955a40f1187c1687d1166
Andrea Francia <andrea@andreafrancia.it>
2013-07-01 12:18:15 +0200

    Aggiunta icona per iTunes

--- build-icons.sh
+++ build-icons.sh
@@ -1,2 +1,3 @@
+convert -flatten -resize 1024x1024 recycle.xcf recycle-1024x1024.png
 convert -flatten -resize 114x114 recycle.xcf recycle@2x.png
 convert -flatten -resize 57x57   recycle.xcf recycle.png
